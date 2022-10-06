e53a58d2d5d6d0fd7909106f0088c6958e7fa3dc
Francis <114366108+fjbaldon@users.noreply.github.com>
2022-10-06 18:24:16 +0800

    First commit

---
+++ school_stuff/assignment_1/four.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) {
+  clrscr();
+  printf("Water Bill Calculator\n");
+  float prev_reading, pres_reading, bill;
+  const float COST_PER_CUBIC_METER = 15.50;
+
+  printf("Enter previous reading in cubic meter: ");
+  scanf("%f", &prev_reading);
+  printf("Enter present reading in cubic meter: ");
+  scanf("%f", &pres_reading);
+  
+  bill = (pres_reading - prev_reading) * COST_PER_CUBIC_METER;
+  printf("Your water bill will be: PHP %.2f\n", bill);
+
+  getch();
+  return 0;
+}
