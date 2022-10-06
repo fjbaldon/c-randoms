58be169eb5fc33d0f07bbe634de85c91670870b1
Francis <114366108+fjbaldon@users.noreply.github.com>
2022-10-06 18:23:37 +0800

    First commit

---
+++ school_stuff/assignment_1/three.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) {
+  clrscr();
+  printf("Difference of 4 Numbers Calculator\n");
+  int num_1, num_2, num_3, num_4, diff;
+
+  printf("Enter first number: ");
+  scanf("%d", &num_1);
+  printf("Enter second number: ");
+  scanf("%d", &num_2);
+  printf("Enter third number: ");
+  scanf("%d", &num_3);
+  printf("Enter fourth number: ");
+  scanf("%d", &num_4);
+  
+  diff = (num_1 + num_2) - (num_3 + num_4);
+  printf("(%d + %d) - (%d + %d) = %d\n", num_1, num_2, num_3, num_4, diff);
+
+  getch();
+  return 0;
+}
