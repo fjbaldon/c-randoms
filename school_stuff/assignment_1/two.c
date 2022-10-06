f7a288db54867e195d83f1893c1d9b8ee6f3c1de
Francis <114366108+fjbaldon@users.noreply.github.com>
2022-10-06 18:23:10 +0800

    First commit

---
+++ school_stuff/assignment_1/two.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>#include <stdio.h>
+#include <conio.h>
+
+int main(void) {
+  clrscr();
+  printf("Difference of 2 Numbers Calculator\n");
+  int num_1, num_2, diff;
+
+  printf("Enter first number: ");
+  scanf("%d", &num_1);
+  printf("Enter second number: ");
+  scanf("%d", &num_2);
+
+  diff = num_1 - num_2;
+  printf("%d - %d = %d\n", num_1, num_2, diff);
+
+  getch();
+  return 0;
+}
+
+int main(void) {
+  clrscr();
+  printf("Difference of 2 Numbers Calculator\n");
+  int num_1, num_2, diff;
+
+  printf("Enter first number: ");
+  scanf("%d", &num_1);
+  printf("Enter second number: ");
+  scanf("%d", &num_2);
+
+  diff = num_1 - num_2;
+  printf("%d - %d = %d\n", num_1, num_2, diff);
+
+  getch();
+  return 0;
+}
