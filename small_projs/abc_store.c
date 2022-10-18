c6fe778474f2d3d26735659a5d89b02405482658
Francis <114366108+fjbaldon@users.noreply.github.com>
2022-10-19 07:28:57 +0800

    Create abc_store.c

---
+++ small_projs/abc_store.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float balls_cashier(int balls) {
+  float ball_price = 4.75f;
+  if (balls <= 10) return ball_price * balls;
+  else if (balls > 10) {
+    float dscount;
+    dscount = (ball_price * balls) * 0.1f;
+    return (ball_price * balls) - dscount;
+  }
+  else return 0;
+}
+
+int main(void) {
+  system("clear");
+  int balls;
+
+  puts("ABC STORE CASHIERING SYSTEM");
+  fputs("\nENTER NUMBER OF BALLS: ", stdout);
+  scanf("%d", &balls);
+  printf("TOTAL: %.2f\n", balls_cashier(balls));
+  puts("\n*** THANK YOU FOR SHOPPING WITH US ***");
+
+  return 0;
+}
