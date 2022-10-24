8b2de8e0641d0f20577dde01c890653b6e326839
Francis <114366108+fjbaldon@users.noreply.github.com>
2022-10-24 15:33:09 +0800

    Update conio.c
    Forgot to add some functions

--- conio.h/conio.c
+++ conio.h/conio.c
@@ -8,6 +8,11 @@ int clrscr(void) {
   system("clear");
   return 0;
 }
+
+int gotoxy(int x, int y) { 
+  printf("%c[%d;%df", 0x1b, y, x);
+  return 0;
+}
  
 int getch(void) {
     struct termios oldattr, newattr;
@@ -15,6 +20,19 @@ int getch(void) {
     tcgetattr( STDIN_FILENO, &oldattr );
     newattr = oldattr;
     newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    system("clear");
+    return ch;
+}
+
+int getche(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON );
     tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
     ch = getchar();
     tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
