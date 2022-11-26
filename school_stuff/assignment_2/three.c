804f8435edc41afae35b9745247401a3b2ba25f9
Francis Baldon <francisjohn.baldon@tutanota.com>
2022-11-26 20:01:27 +0800

    Clean up

---
+++ school_stuff/assignment_2/three.c
@@ -0,0 +1,20 @@
+//3.	Design a program that will display which of the 3 number inputs is bigger
+#include <stdio.h>
+
+int main(void) {
+  /**           **/
+  int a, b, c;
+
+  printf("Enter first number: ");
+  scanf("%d", &a);
+
+  printf("Enter second number: ");
+  scanf("%d", &b);
+
+  printf("Enter third number: ");
+  scanf("%d", &c);
+
+  /**             **/
+  gethc();
+  return 0;
+}
