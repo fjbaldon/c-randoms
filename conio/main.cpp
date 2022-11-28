8b234400c73a5c62e9e42e6e61c48a11b4a248b3
Francis Baldon <francisjohn.baldon@tutanota.com>
2022-11-28 09:08:14 +0800

    #oyao idol

--- conio/main.cpp
+++ conio/main.cpp
@@ -1,33 +1,20 @@
 #include <stdio.h>
-#include <conio.h>
+#include "conio.h"
 
-int main() {
-  //No.1 Final bill of a water consumer
-  float preread, prevread, reading, parbill, ampmeter, fbill, tax;
+main() {
+  clrscr();
+  int a, b, c, ab, abc; 
 
-  printf("\nEnter present reading: ");
-  scanf("%f",&preread);
-  printf("\nEnter previous reading: ");
-  scanf("%f",&prevread);
-  printf("\nEnter amount per cubic meter: ");
-  scanf("%f",&ampmeter);
-  //compute 
-  reading = preread - prevread;
-  parbill = reading * ampmeter;
+  printf("Enter first number:  ");
+  scanf("%d", &a);
+  printf("Enter second number: ");
+  scanf("%d", &b);
+  printf("Enter third number:  ");
+  scanf("%d", &c);
 
-  if (reading < 10) {
-    tax = parbill *.10;
-  } else if(reading >=10 && reading <=25) {
-    tax = parbill *.15;       
-  } else if(reading >=26 && reading <=45) {
-    tax = parbill *.20;
-  } else if(reading >=46 && reading <=60) {
-    tax = parbill *.25; 
-  } else {
-    tax = parbill *.30;   
-  }
-  //compute final bill
-  fbill = parbill + tax;
-  printf("\nFinal Bill of the Consumer is: %.2f",fbill);
-  return 0;
+  abc = (ab = a > b ? a : b) > c ? ab : c;
+  
+  printf("\nThe largest number is %d.\n", abc);
+
+  getch();
 }
